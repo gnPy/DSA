5e638a0245bc83e3e2e1f3571d7149d7187c8c34
Gautham N Pai <gauthamnpai2022@gmail.com>
2024-02-13 18:49:03 +0530

    updated_seperated_signed_nums

--- PROGRAMS/LINKED LIST/seperateNegPos.c
+++ PROGRAMS/LINKED LIST/seperateNegPos.c
@@ -1,67 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
-    int val;
-    struct node *next;
-};
+struct node{
+    int val ; 
+    struct node *next ; 
+} *pos = NULL , *neg = NULL; 
 
-struct node *createPoly() {
-    int n;
-    struct node *pos = NULL, *neg = NULL, *temp1 = NULL, *temp2 = NULL;
-    printf("ENTER NUMBER OF ELEMENTS: ");
-    scanf("%d", &n);
-    printf("ENTER ELEMENTS: ");
-    for (int i = 0; i < n; i++) {
-        int x;
-        scanf("%d", &x);
-        struct node *temp = malloc(sizeof(struct node));
-        temp->val = x;
-        temp->next = NULL;
-        if (x < 0) {
-            if (neg == NULL) {
-                neg = temp;
-                temp2 = neg;
-            } else {
-                temp2->next = temp;
-                temp2 = temp2->next;
+struct node *create(struct node *pos , struct node *neg){
+    struct node *temp1 = pos , *temp2 = neg ; 
+
+    int n; 
+    printf("ENTER THE NUMBER OF ELEMENTS : ") ; 
+    scanf("%d" , &n) ; 
+    printf("ENTER THE ELEMENTS  : ") ; 
+    for( int i = 0 ; i< n ; i++){
+        int x ; 
+        scanf("%d" , &x) ; 
+
+        struct node *ptr = malloc(sizeof(struct node )) ; 
+        ptr->val = x ; 
+        ptr->next = NULL ; 
+
+        if(x >= 0){
+            if(temp1 == NULL){
+                pos = ptr ; 
+                temp1 = ptr ; 
             }
-        } else {
-            if (pos == NULL) {
-                pos = temp;
-                temp1 = pos;
-            } else {
-                temp1->next = temp;
-                temp1 = temp1->next;
+            else{
+                temp1->next = ptr ; 
+                temp1 = ptr ; 
+            }
+        }
+        else{
+            if(temp2 == NULL){
+                neg = ptr ; 
+                temp2 = ptr ; 
+            }
+            else{
+                temp2->next = ptr ; 
+                temp2 = ptr ; 
             }
         }
     }
-    if (neg != NULL && pos != NULL) {
-        temp2->next = pos;
-        return neg;
-    } 
-    
-    else if (neg != NULL) {
-        temp2->next = neg;
-        return neg;
-    } 
-    else {
-        temp1->next = pos;
-        return pos;
+    if( temp2 != NULL && temp1 != NULL){
+        temp2->next = pos ; 
+        return neg ; 
+    }
+    else if( temp1 == NULL){
+        return neg ; 
+    }
+    else{
+        return pos ; 
     }
 }
 
-void display(struct node *head) {
-    struct node *temp = head;
-    while(temp != NULL) {
-        printf("%d ", temp->val);
-        temp = temp->next;
+void display(struct node *head){
+    struct node *temp = head ; 
+    while(temp != NULL){
+        printf("%d " , temp->val) ; 
+        temp = temp->next ; 
     }
     printf("\n") ; 
 }
 
-int main() {
-    struct node *neg = createPoly();
-    display(neg);
-    return 0;
+int main(){
+    pos = create(pos , neg) ; 
+    display(pos) ;
 }
